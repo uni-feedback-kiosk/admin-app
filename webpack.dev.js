909f6c7fd81973ba49652985ffee809fbcc31a47
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2023-01-15 14:36:24 +0300

    Tweak webpack config

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,7 @@ const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'development',
+  devtool: 'eval-source-map',
   devServer: {
     static: path.join(__dirname, 'src'),
     historyApiFallback: true,
